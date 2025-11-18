e750baf7526d1d7048fa4dbec8bbd0d3d2e787b6
Josh Dittmer <joshua.dittmer@sjsu.edu>
2025-11-18 15:35:45 -0800

    force homepage dynamic

--- frontend/src/app/home/dashboard/page.tsx
+++ frontend/src/app/home/dashboard/page.tsx
@@ -3,7 +3,7 @@ import ItemSlider from '@/components/item_slider/item_slider.client';
 import { ItemsByCategoryResponse } from "@/lib/api/models";
 import { get, request } from "@/lib/api/request";
 
-
+export const dynamic = 'force-dynamic';
 
 export default async function DashboardPage() {
     // Request items grouped by category from flexible backend endpoint
