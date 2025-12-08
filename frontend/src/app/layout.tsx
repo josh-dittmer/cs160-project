bf6ef3d678abc99595f3fc1696eedc1321d474ea
Ziqing Jiang <ziqing.jiang@sjsu.edu>
2025-12-08 11:05:46 -0800

    fix bug: mode consistency

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Script from "next/script";
+import { ThemeProvider } from "@/contexts/theme";
 
 const geistSans = Geist({
     variable: "--font-geist-sans",
@@ -32,11 +33,12 @@ export default function RootLayout({
             <body
                 className={`${geistSans.variable} ${geistMono.variable} antialiased`}
             >
-                <Provider>
-                    {children}
-                </Provider>
+                <ThemeProvider>
+                    <Provider>
+                        {children}
+                    </Provider>
+                </ThemeProvider>
             </body>
         </html>
     );
 }
-
